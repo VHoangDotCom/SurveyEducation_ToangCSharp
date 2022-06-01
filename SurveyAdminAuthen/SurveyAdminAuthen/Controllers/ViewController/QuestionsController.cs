22d1021968af46e686b7899cba2f42391fbc588e
Nguyen Viet Hoang (APTECH HN) <hoangnvth2010033@fpt.edu.vn>
2022-06-02 06:37:38 +0700

    Admin Controller

---
+++ SurveyAdminAuthen/SurveyAdminAuthen/Controllers/ViewController/QuestionsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SurveyAdminAuthen.Models;
+using SurveyAdminAuthen.Models.Entity;
+
+namespace SurveyAdminAuthen.Controllers.ViewController
+{
+    public class QuestionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Questions
+        public ActionResult Index()
+        {
+            var questions = db.Questions.Include(q => q.Survey);
+            return View(questions.ToList());
+        }
+
+        // GET: Questions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // GET: Questions/Create
+        public ActionResult Create()
+        {
+            ViewBag.SurveyID = new SelectList(db.Surveys, "ID", "Title");
+            return View();
+        }
+
+        // POST: Questions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,QuestionText,Required,QuestionType,SurveyID")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Questions.Add(question);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SurveyID = new SelectList(db.Surveys, "ID", "Title", question.SurveyID);
+            return View(question);
+        }
+
+        // GET: Questions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SurveyID = new SelectList(db.Surveys, "ID", "Title", question.SurveyID);
+            return View(question);
+        }
+
+        // POST: Questions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,QuestionText,Required,QuestionType,SurveyID")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(question).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SurveyID = new SelectList(db.Surveys, "ID", "Title", question.SurveyID);
+            return View(question);
+        }
+
+        // GET: Questions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question question = db.Questions.Find(id);
+            if (question == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question);
+        }
+
+        // POST: Questions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Question question = db.Questions.Find(id);
+            db.Questions.Remove(question);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SurveyAdminAuthen/SurveyAdminAuthen/Models/Entity/AnswerSubmit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SurveyAdminAuthen.Models.Entity
+{
+    public class AnswerSubmit
+    {
+        [Key]
+        public int ID { get; set; }
+
+        //Foreign key
+        public int QuestionID { get; set; }
+        public virtual Survey Survey { get; set; }
+
+        public ICollection<TextAnswer> TextAnswers { get; set; }
+
+        public AnswerSubmit()
+        {
+            this.TextAnswers = new HashSet<TextAnswer>();
+        }
+    }
+}
